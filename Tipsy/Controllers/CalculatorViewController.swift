acbfe8339f2f4b3035fb9576ac2518d111ffa162
Miguel Sierra <miguelsierrac@gmail.com>
2021-01-29 22:29:59 -0500

    Step 5: Figure out how Many People to Split the Bill Between

--- Tipsy/Controllers/CalculatorViewController.swift
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -16,28 +16,31 @@ class CalculatorViewController: UIViewController {
     @IBOutlet weak var twentyPctButton: UIButton!
     @IBOutlet weak var splitNumberLabel: UILabel!
     
-    private var tipSelected = 0.0
+    private var tipSelected = 0.1
+    private var splitNumberSelected = 2
     
     @IBAction func tipChanged(_ sender: UIButton) {
         zeroPctButton.isSelected = sender == zeroPctButton
         tenPctButton.isSelected = sender == tenPctButton
         twentyPctButton.isSelected = sender == twentyPctButton
         
-        if sender == zeroPctButton {
+        if zeroPctButton.isSelected {
             tipSelected = 0.0
-        } else if sender == tenPctButton {
+        } else if tenPctButton.isSelected {
             tipSelected = 0.1
-        } else if sender == twentyPctButton {
+        } else if twentyPctButton.isSelected {
             tipSelected = 0.2
         }
     }
     
     @IBAction func stepperValueChanged(_ sender: UIStepper) {
-        
+        splitNumberSelected = Int(sender.value)
+        splitNumberLabel.text = "\(splitNumberSelected)"
     }
     
     @IBAction func calculatePressed(_ sender: UIButton) {
-        print("\(tipSelected)")
+        print("Tip: \(tipSelected)")
+        print("Split Number: \(splitNumberSelected)")
     }
 }
 
