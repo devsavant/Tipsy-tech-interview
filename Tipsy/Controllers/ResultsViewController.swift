98f9e867d0e5277d044c9ada48a7cab47cbe8f83
Miguel Sierra <miguelsierrac@gmail.com>
2021-01-29 21:39:28 -0500

    Step 3: Create IBActions and IBOutlets for the Results Screen

---
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by Miguel Mauricio Sierra on 29/01/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func recalculatePressed(_ sender: UIButton) {
+    }
+    
+}
