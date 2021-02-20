7e0927359aa5a5d0b4d339d68d2f03f1a895941d
jp-vargas <vargasm.jp@gmail.com>
2021-02-20 07:10:43 -0500

    fix billing

---
+++ Tipsy/Constants/Constants.swift
@@ -0,0 +1,15 @@
+//
+//  Constants.swift
+//  Tipsy
+//
+//  Created by Juan Vargas on 20/02/21.
+//  Copyright © 2021 The App Brewery. All rights reserved.
+//
+
+import Foundation
+
+
+struct Constants {
+    static let NO_BILL = "Please enter the bill total"
+    static let WRONG_NUMBER = "Please put a valid number"
+}
